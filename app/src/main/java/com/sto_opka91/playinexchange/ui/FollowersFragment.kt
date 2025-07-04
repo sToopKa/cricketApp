6c75f918081235bec1f2dd0347607550945ea751
stoopka <stepanbulanov1991@gmail.com>
2025-07-04 05:28:27 +0300

    new

--- app/src/main/java/com/sto_opka91/playinexchange/ui/FollowersFragment.kt
+++ app/src/main/java/com/sto_opka91/playinexchange/ui/FollowersFragment.kt
@@ -1,20 +1,28 @@
 package com.sto_opka91.playinexchange.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.sto_opka91.playinexchange.R
 import com.sto_opka91.playinexchange.databinding.FragmentFollowersBinding
 import com.sto_opka91.playinexchange.databinding.FragmentMatchesBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class FollowersFragment : Fragment() {
 
     private var _binding: FragmentFollowersBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,14 +36,32 @@ class FollowersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getUser()
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+        observeViewModel()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.settingsState.collect { state ->
+                if(state.user != null && state.user.isNotEmpty()) {
+
+                    val user = state.user[0]
+
+                    if(user.name.isNotEmpty()){
+                        binding.tvName.text = user.name
+                    }
+                    
+                }
+            }
+        }
     }
 
 
